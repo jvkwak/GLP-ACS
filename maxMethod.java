53961a2624d74565fcabf808b2351fbb05744f33
jvkwak <48267673+jvkwak@users.noreply.github.com>
2019-03-08 14:12:22 +0900

    Update maxMethod.java

--- maxMethod.java
+++ maxMethod.java
@@ -1,10 +1,11 @@
 // Returns the largest value in the given array.
-public static int max(int data[10]) {
-    int max = 0;
-    for (int i = 0; i < data[].length(); i++) {
-        if (array[i] > max) {
-            max = array[i];
-        }
-    }
-    return max[];
-}
+public static int max(int[] data) {
+	    int max = 0;
+	    for (int i = 0; i < data.length; i++) {
+	        if (data[i] > max) {
+	            max = data[i];
+	        }
+	    }
+	    return max;
+	
+	}
